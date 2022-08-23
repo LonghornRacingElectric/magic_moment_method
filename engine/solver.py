a7eef834f8126073c2afeaea98701136e0127865
darenau03 <darren.au@utexas.edu>
2022-08-22 23:12:45 -0500

    added cleaner logging

--- engine/solver.py
+++ engine/solver.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import numpy as np
 from scipy.optimize import fsolve
 from copy import copy
@@ -5,23 +7,34 @@ import warnings
 import engine
 import vehicle_params
 import logging
+import os
+
 
 class Solver:
-    def __init__(self, vehicle_parameters:vehicle_params.BaseVehicle, initial_guess:dict = None):
+
+    def __init__(self, vehicle_parameters: vehicle_params.BaseVehicle, initial_guess: dict = None):
         """_summary_
 
         Args:
             vehicle_parameters (vehicle_params._parameter_file_): specific static & initial vehicle parameters
             initial_guess (dict, optional): dictionary of 6 solver initial dependent parameter guesses. Defaults to None.
         """
-        self.__initial_guess = [initial_guess[x] for x in self.__output_variable_names] if initial_guess else [0, 0, 0, 0, 0, 0]
+        self.__initial_guess = [initial_guess[x] for x in self.__output_variable_names] if initial_guess else [0, 0, 0,
+                                                                                                               0, 0, 0]
         self.vehicle = engine.Vehicle(vehicle_parameters)
-        logging.basicConfig(filename='test.log', level=logging.DEBUG,
+        self.createLogFile()
+
+    def createLogFile(self):
+        PATH = datetime.now().strftime('logs\log_%d_%m_%Y')
+        i = 0
+        while os.path.exists(PATH + "_%s.log" % i):
+            i += 1
+
+        logging.basicConfig(filename=PATH + "_%s.log" % i, level=logging.DEBUG,
                             format='%(asctime)s:%(levelname)s:%(message)s')
         logging.debug('NEW SOLVE: ')
 
-
-    def solve(self, input_state:engine.State):
+    def solve(self, input_state: engine.State):
         """
         Non-linear solve for unique output variable set to match the prescribed states, with an initial guess of dependent states
         Converges on dependent vehicle states by iterating dependent parameter guesses
@@ -40,10 +53,8 @@ class Solver:
         # allow up to 5 chances for convergence - above 20 doesnt lead to many additional convergences
         guesses_allowed = 5
 
-
-
         for i in range(guesses_allowed):
-            results = fsolve(self.__DOF6_motion_residuals, self.__initial_guess, full_output = True)
+            results = fsolve(self.__DOF6_motion_residuals, self.__initial_guess, full_output=True)
             if results[2] == 1:
                 if i != 0:
                     logging.debug("Solution converged after changing initial guess")
@@ -56,8 +67,9 @@ class Solver:
                     # print(f"Solution convergence not found after {guesses_allowed} guesses for state: {input_state.body_slip} {input_state.s_dot} {input_state.steered_angle}")
                     logging.debug("Solution convergence not found after {} guesses for state: "
                                   "{} {} {}"
-                                  .format(guesses_allowed,input_state.body_slip,input_state.s_dot,input_state.steered_angle))
-                    #print(results[1]["fvec"],"\n") # for debugging why the solution didnt converge
+                                  .format(guesses_allowed, input_state.body_slip, input_state.s_dot,
+                                          input_state.steered_angle))
+                    # print(results[1]["fvec"],"\n") # for debugging why the solution didnt converge
                     return {}
                 self.__initial_guess[self.__output_variable_names.index("heave")] += 0.00125
 
@@ -65,8 +77,7 @@ class Solver:
     def __output_variable_names(self):
         return ["heave", "x_double_dot", "y_double_dot", "yaw_acceleration", "roll", "pitch"]
 
-
-    def __DOF6_motion_residuals(self, x:list):
+    def __DOF6_motion_residuals(self, x: list):
         """
         Calculates six residuals for six degree of freedom vehicle, finds dependent state variables based on input guess.
         Used by non-linear solver to converge on dependent vehicle state variables.
@@ -89,7 +100,6 @@ class Solver:
         vehicle_forces_ntb = self.vehicle.intermediate_frame_to_ntb_transform(forces)
         vehicle_moments_ntb = self.vehicle.intermediate_frame_to_ntb_transform(moments)
 
-
         # TODO: CoG movements due to roll / pitch / heave
         """ 
         NOTE: 1) ~~Kinetic Moments~~
@@ -108,7 +118,6 @@ class Solver:
         # F = m * a
         inertial_forces = self.vehicle.get_inertial_forces(translation_accelerations_ntb)
         summation_forces = inertial_forces - vehicle_forces_ntb
-
 
         # log dependent states
         [self.vehicle.logger.log(self.__output_variable_names[i], x[i]) for i in range(len(x))]
