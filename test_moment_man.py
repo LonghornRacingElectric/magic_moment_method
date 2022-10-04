946b751cd752550e908284d7f7f368596b9b2003
kierancoz <kierancoz.w@gmail.com>
2022-10-04 16:51:44 -0500

    test

--- test_moment_man.py
+++ test_moment_man.py
@@ -19,14 +19,19 @@ for index, tire in enumerate(tires):
     normal_force = result[tire + "_tire_tire_centric_forces_2"]
     long_force = result[tire + "_tire_tire_centric_forces_0"]
     lat_force = result[tire + "_tire_tire_centric_forces_1"]
-    output = [solver.vehicle.suspension._Suspension__tires.rear_left.comstock(slip, -slip_angle, normal_force, 0) for slip in slip_ratios] # 
-    #output2 = [solver.vehicle.suspension._Suspension__tires.front_left.comstock(10, slip/100, normal_force, 0) for slip in slip_ratios] # 
-    axs_i.plot(slip_ratios, output)
-    axs_i.plot(slip_ratios, [solver.vehicle.suspension._Suspension__tires.rear_left.longitudinal_pacejka(normal_force, slip) for slip in slip_ratios])
-    axs_i.plot(slip_ratios, [solver.vehicle.suspension._Suspension__tires.rear_left.lateral_pacejka(0, normal_force, slip) for slip in slip_ratios])
+    if "front" in tire:
+        comstock_output = [solver.vehicle.suspension._Suspension__tires.front_left.comstock(slip, -slip_angle, normal_force, 0) for slip in slip_ratios]
+        long_paj = [solver.vehicle.suspension._Suspension__tires.front_left.longitudinal_pacejka(normal_force, slip) for slip in slip_ratios]
+        lat_paj = [solver.vehicle.suspension._Suspension__tires.front_left.lateral_pacejka(0, normal_force, slip) for slip in slip_ratios]
+    else:
+        comstock_output = [solver.vehicle.suspension._Suspension__tires.rear_left.comstock(slip, -slip_angle, normal_force, 0) for slip in slip_ratios]
+        long_paj = [solver.vehicle.suspension._Suspension__tires.rear_left.longitudinal_pacejka(normal_force, slip) for slip in slip_ratios]
+        lat_paj = [solver.vehicle.suspension._Suspension__tires.rear_left.lateral_pacejka(0, normal_force, slip) for slip in slip_ratios]
+    axs_i.plot(slip_ratios, comstock_output)
+    axs_i.plot(slip_ratios, long_paj)
+    axs_i.plot(slip_ratios, lat_paj)
     axs_i.scatter(slip_ratio, long_force)
     axs_i.scatter(slip_angle, lat_force)
-    #axs_i.plot(slip_ratios, output2)
     axs_i.grid()
     axs_i.legend(["FX_com", "FY_com", "actual_FZ", "pure_long", "pure_lateral", "actual_SR", "actual_SA"])
     axs_i.set_title(tire)
