40453f7ad244d1932deb2af93b27ad46dd8987c4
kierancoz <kierancoz.w@gmail.com>
2022-10-08 17:42:27 -0500

    fix unit_testing

--- tests/unit_testing.py
+++ tests/unit_testing.py
@@ -5,12 +5,10 @@ import warnings
 import argparse
 warnings.filterwarnings('ignore', 'The iteration is not making good progress')
 
-from pathlib import Path
 import sys
-path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
-sys.path.insert(0, path)
-import engine
-import vehicle_params
+sys.path.append("..")
+import magic_moment_method.vehicle_params as vehicle_params
+import magic_moment_method.state_solver as state_solver
 
 s_dot_sweep = [15]
 steering_sweep = [-0.07, 0, 0.07]
@@ -18,7 +16,7 @@ body_sweep = [-0.07, 0, 0.07]
 torque_sweep = [0]
 bias_sweep = [True]
 reference_file = "tests/test_MMM.csv"
-solver = engine.Solver(vehicle_params.UnitTestCar())
+solver = state_solver.Solver(vehicle_params.UnitTestCar())
 
 @pytest.mark.parametrize("s_dot", s_dot_sweep)
 @pytest.mark.parametrize("steered_angle", steering_sweep)
@@ -26,7 +24,7 @@ solver = engine.Solver(vehicle_params.UnitTestCar())
 @pytest.mark.parametrize("torque_request", torque_sweep)
 @pytest.mark.parametrize("is_left_bias", bias_sweep)
 def test_josie_solver(s_dot, steered_angle, body_slip, torque_request, is_left_bias):
-    o_d = solver.solve(engine.State(body_slip, steered_angle, s_dot, torque_request, is_left_bias))
+    o_d = solver.solve(state_solver.State(body_slip, steered_angle, s_dot, torque_request, is_left_bias))
     e_d = pd.read_csv(reference_file)
     e_d_filtered = e_d[(e_d["body_slip"] == body_slip) & (e_d["s_dot"] == s_dot) & (e_d["steered_angle"] == steered_angle)].iloc[0]
     for key, value in e_d_filtered.items():
